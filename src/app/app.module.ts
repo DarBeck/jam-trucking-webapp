b7307005694585af3358098626237a03fc418eab
Darren Beckford <darren.beckford@hotmail.com>
2023-10-29 13:57:09 -0500

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './main/pages/login/login.component';
+import { RegisterComponent } from './main/pages/register/register.component';
+import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
