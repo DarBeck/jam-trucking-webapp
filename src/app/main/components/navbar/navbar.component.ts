67891f7a859bc2a0f4ef1b2c2d8455e7565dab9f
Darren Beckford <darren.beckford@hotmail.com>
2023-11-08 00:58:47 -0500

    Modify user on nav

--- src/app/main/components/navbar/navbar.component.ts
+++ src/app/main/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user: User | null = null;
+  userImage: string = '/assets/images/profile-image-placeholder.png';
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.user = this.userService.userData;
+  }
+
+  logout() {
+    this.userService.updateUser(null);
+    localStorage.removeItem('user');
+    this.router.navigateByUrl('/login');
+  }
 }
